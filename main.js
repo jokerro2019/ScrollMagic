bc579d2d48f4bd8276eca65d963b13f2124be1fb
Joker <denis.atompark@gmail.com>
2019-12-03 14:38:04 +0200

    asdasd

---
+++ main.js
@@ -0,0 +1,22 @@
+
+(function($) {
+
+    // Init ScrollMagic
+    var ctrl = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            triggerHook: 'onLeave'
+        }
+    });
+
+    $("section").each(function() {
+
+        new ScrollMagic.Scene({
+            triggerElement: this
+        })
+            .setPin(this)
+            .addTo(ctrl);
+
+    });
+
+})(jQuery);
+
